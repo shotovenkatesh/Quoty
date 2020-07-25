ed7007c5bc89b6dd0bbdf3016e659c90dd139e75
Venkatesh <shotovenkatesh705@gmail.com>
2020-07-25 23:24:42 +0530

    Initial Commit

---
+++ Quoty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quoty
+//
+//  Created by Venkatesh on 25/07/20.
+//  Copyright © 2020 Venkatesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
